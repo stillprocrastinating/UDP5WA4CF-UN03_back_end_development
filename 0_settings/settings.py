67bdaaad06355203b353bbc72df4aed7e137a69e
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-01-15 21:45:18 +0000

    SECRET_KEY = os.environ.get('SECRET_KEY')

--- 0_settings/settings.py
+++ 0_settings/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@z3^vvqa+a&$cif7z_=@d@njz@5l&ru8s8o$y%gr5f8__r6g-)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
