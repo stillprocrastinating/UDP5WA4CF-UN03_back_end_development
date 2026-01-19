fabbda65319e77f05a794f4873a9d4eed61bc0a4
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-01-19 20:17:33 +0000

    template setup

--- 0_settings/settings.py
+++ 0_settings/settings.py
@@ -19,6 +19,8 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, '0_templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -62,7 +64,9 @@ ROOT_URLCONF = '0_settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
