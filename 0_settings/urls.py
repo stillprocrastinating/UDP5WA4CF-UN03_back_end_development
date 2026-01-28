6ed3b024e98aa9b79f28c7aa0e18d61e863b23f7
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-01-28 21:56:06 +0000

    python manage.py startapp test

--- 0_settings/urls.py
+++ 0_settings/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', include('test.urls'), name='test-urls'),
     path('', include('question.urls'), name='question-urls'),
 ]
