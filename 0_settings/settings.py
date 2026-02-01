624968080dd90a4668ff5f0b27b161e8cd01f8a8
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-02-01 20:03:49 +0000

    header style upgrade #2

--- 0_settings/settings.py
+++ 0_settings/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '0_static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '0_static'),
