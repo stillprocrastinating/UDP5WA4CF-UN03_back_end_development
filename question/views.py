4eeabe2c84693b0ca39790439ba0ca8801cae6f2
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-02-14 17:11:58 +0000

    BUGFIX question_detail.html

--- question/views.py
+++ question/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+#from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, reverse
 from django.views import generic
 from .models import Question
 
 
 class QuestionList(generic.ListView):
     queryset = Question.objects.all()
+
+
+def question_detail(request, slug):
+    """
+    Display an individual :model:`question.Question`.
+
+    :param request: The requested question.
+    :param slug: The identification (slug) of the request.
+    """
+
+    queryset = Question.objects.all()
+    question = get_object_or_404(queryset, slug=slug)
+
+    context = {
+        "question": question,
+    }
+
+    return render(
+        request,
+        "question/question_detail.html",
+        context,
+    )
