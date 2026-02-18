939b6db130617237387e74f227567849140defa6
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-02-18 23:49:33 +0000

    .new-button{}

--- question/urls.py
+++ question/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.QuestionList.as_view(), name='questions'),
     path('<slug:slug>/', views.question_detail, name='question_detail'),
+    path('', views.QuestionList.as_view(), name='questions'),
 ]
--- test/urls.py
+++ test/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('test/', views.TestList.as_view(), name='tests'),
+    #path('<slug:slug>/', views.test_detail, name='test_detail'),
+    #path('new/', views.test_new, name='test-new'),     # forms.py & test_new.html
+    path('', views.TestList.as_view(), name='tests'),
 ]
