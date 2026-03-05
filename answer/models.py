bf6047da5195df67bd6393e74739f64521717a84
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-03-05 14:44:14 +0000

    BROKEN Answer() > separated answers

--- answer/models.py
+++ answer/models.py
@@ -18,19 +18,21 @@ class Answer(models.Model):
     """
 
     id = models.AutoField(primary_key=True)
-    question_id = models.ForeignKey(
-        to=Question,
-        related_name="question_answers",
-        on_delete=models.SET(str(Question.question))
-    )
-    test_id = models.ForeignKey(
-        to=Test,
-        related_name="test_answers",
-        on_delete=models.SET(str(Test.id))
-    )
+    question_id = models.ForeignKey(Question, related_name="question_answers", on_delete=models.SET(str(Question.question)))
+    test_id = models.ForeignKey(Test, related_name="test_answers", on_delete=models.SET(str(Test.id)))
     # option = models.IntegerField()
-    correct_option_frequency = models.IntegerField()
-    incorrect_option_frequency = models.IntegerField()
+    answer1 = models.IntegerField(default=0)
+    answer2 = models.IntegerField(default=0)
+    answer3 = models.IntegerField(default=0)
+    answer4 = models.IntegerField(default=0)
+    answer5 = models.IntegerField(default=0)
+    answer1_percentage = models.Expression(answer1 / Test.participant_number)
+    answer2_percentage = models.Expression(answer2 / Test.participant_number)
+    answer3_percentage = models.Expression(answer3 / Test.participant_number)
+    answer4_percentage = models.Expression(answer4 / Test.participant_number)
+    answer5_percentage = models.Expression(answer5 / Test.participant_number)
+    # Use Question.sub_correct_answer_individual to get the correct answer
+    # Create a warning identifier using the highest percentage answer
     tester = models.ForeignKey(User, on_delete=models.SET(str(User.username)))
     teaching_warning = models.IntegerField(choices=TEACHING_WARNING, default=0)
 
