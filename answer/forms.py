bf004d7665e73dbafc28697024d8a25daf5d0b85
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-03-06 09:02:05 +0000

    comment everything out -> python manage.py runserver

--- answer/forms.py
+++ answer/forms.py
@@ -6,14 +6,15 @@ class AnswerNew(forms.ModelForm):
     """
     Generates the form to create Answers
     """
-    q_answers = forms.ModelMultipleChoiceField(
-        queryset=Question.objects.filter(Test.t_questions),     # fix to have Question.answerx labels
-        widget=forms.CheckboxSelectMultiple
-    )
+    # q_answers = forms.ModelMultipleChoiceField(
+    #     queryset=Question.objects.filter(Test.t_questions),     # fix to have Question.answerx labels
+    #     widget=forms.CheckboxSelectMultiple
+    # )
 
     class Meta:
         model = Answer
-        fields = ('question_id', 'test_id', 'option', 'correct_option_frequency', 'incorrect_option_frequency')
+        # fields = ('question_id', 'test_id', 'option', 'correct_option_frequency', 'incorrect_option_frequency')
+        fields = ('id',)
 
     def __str__(self):
         return Answer.correct_option_frequency
