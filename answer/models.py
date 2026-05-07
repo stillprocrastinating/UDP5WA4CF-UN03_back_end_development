98cf33682afa6868e5cbc8acc21bfb4352d01b18
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-05-07 20:44:37 +0100

    FIX UCLSans-Light

--- answer/models.py
+++ answer/models.py
@@ -130,15 +130,15 @@ class Answer(models.Model):
 
     @property
     def participant_answer(self):
-        if (self.answer1 == 1):
+        if (self.answer1 != 0):
             pa = self.answer1
-        elif (self.answer2 == 1):
+        elif (self.answer2 != 0):
             pa = self.answer2
-        elif (self.answer3 == 1):
+        elif (self.answer3 != 0):
             pa = self.answer3
-        elif (self.answer4 == 1):
+        elif (self.answer4 != 0):
             pa = self.answer4
-        elif (self.answer5 == 1):
+        elif (self.answer5 != 0):
             pa = self.answer5
         else:
             pa = "No answer"
