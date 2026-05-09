a5e305a9719e5a6cb7197ff0737b1a9d53d93625
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-05-09 10:23:13 +0100

    BROKEN answer1(name=Question.answer1)

--- answer/models.py
+++ answer/models.py
@@ -27,7 +27,9 @@ class Answer(models.Model):
         Test, related_name="test_answers", on_delete=models.CASCADE
     )
     # option = models.IntegerField()
-    answer1 = models.IntegerField(default=0)
+    answer1 = models.IntegerField(default=0,
+                                #   name=a1n
+                                  )
     answer2 = models.IntegerField(default=0)
     answer3 = models.IntegerField(default=0)
     answer4 = models.IntegerField(default=0)
@@ -52,6 +54,10 @@ class Answer(models.Model):
     @property
     def tester(self):
         return self.test.tester
+    
+    @property
+    def a1n(self):
+        return self.question.answer1
 
     @property
     def answer1_percentage(self):
