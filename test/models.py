03c58e1bc67132e912884ba6bc1c894d39f4d025
stillprocrastinating <elise.o.hitchcock@outlook.com>
2026-05-20 09:12:16 +0100

    .correct{}

--- test/models.py
+++ test/models.py
@@ -35,9 +35,9 @@ class Test(models.Model):
         choices=T_DIFFICULTY, default=0, verbose_name="test difficulty"
     )
     t_questions = models.ManyToManyField(to=Question)
-    question = models.ForeignKey(
-        Question, on_delete=models.DO_NOTHING, related_name="test_questions"
-    )
+    # question = models.ForeignKey(
+    #     Question, on_delete=models.DO_NOTHING, related_name="test_questions"
+    # )
 
     class Meta:
         ordering = ["date"]
